35c2b2d5abd01d7c42118b1ca67761a02125b202
David Eidelman <eidelman.david@gmail.com>
2023-07-08 11:48:06 -0400

    added htmx

--- noted/server.go
+++ noted/server.go
@@ -5,35 +5,54 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"net/url"
 	"net/http"
 	"text/template"
 
 	"github.com/rs/zerolog/log"
 )
+
 var (
-	//go:embed static/templates/index.gohtml
+	//go:embed all:templates
 	res   embed.FS
 	pages = map[string]string{
-		"/": "static/templates/index.gohtml",
+		"/": "templates/index.gohtml",
 	}
+	//go:embed all:static
+	static embed.FS
+
+	//go: embed all:static/js
+	// jsAssets embed.FS
 )
 
 func InitServer() {
 	log.Info().Msg("initializing server")
-	http.HandleFunc("/", getIndex)
-	http.HandleFunc("/hello", getHello)
-	http.HandleFunc("/api/hello", getGreeting)
-	log.Info().Msg("handlers installed")
-	log.Info().Msg("file server installing")
-	http.FileServer(http.FS(res))
-	log.Info().Msg("server initialized")
-
-	err := http.ListenAndServe(":5555", nil)
+	RegisterFileServer()
+	RegisterHandlers()
+	err := http.ListenAndServe(":5823", nil)
 	if errors.Is(err, http.ErrServerClosed) {
 		log.Info().Msg("server closed")
 	} else if err != nil {
-		log.Fatal().Msg(fmt.Sprintf("unable to start server: %s", err))
+		log.Warn().Msg(fmt.Sprintf("unable to start server: %s", err))
+		panic(1)
 	}
+}
+
+func RegisterFileServer(){
+	fs := http.FileServer(http.FS(static))
+	http.Handle("/static/", fs)
+	log.Info().Msg("static server initialized")
+	log.Debug().Msg("file server installing")
+	http.FileServer(http.FS(res))
+	log.Info().Msg("file server initialized")
+}
+
+func RegisterHandlers() {
+	http.HandleFunc("/", getIndex)
+	http.HandleFunc("/hello", getHello)
+	http.HandleFunc("/api/hello", getGreeting)
+	http.HandleFunc("/trigger_delay", getTriggerDelay)
+	log.Info().Msg("handlers installed")
 }
 
 func getIndex(writer http.ResponseWriter, request *http.Request) {
@@ -42,10 +61,10 @@ func getIndex(writer http.ResponseWriter, request *http.Request) {
 		writer.WriteHeader(http.StatusNotFound)
 		return
 	}
-	log.Info().Msg(fmt.Sprintf("about to process template: %s", page))
+	log.Debug().Msg(fmt.Sprintf("about to process template: %s", page))
 	tpl, err := template.ParseFS(res, page)
 	if err != nil {
-		log.Info().Msg(fmt.Sprintf("page %s not found in pages cache...", request.RequestURI))
+		log.Warn().Msg(fmt.Sprintf("page %s not found in pages cache...", request.RequestURI))
 		writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -53,7 +72,7 @@ func getIndex(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "text/html")
 	writer.WriteHeader(http.StatusOK)
 	data := map[string]interface{}{
-		"userAgent": request.UserAgent(),
+		"name": "David",
 	}
 	if err := tpl.Execute(writer, data); err != nil {
 		log.Info().Msg(fmt.Sprintf("error encountered in executing template: %s", err))
@@ -61,11 +80,27 @@ func getIndex(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 
+func getTriggerDelay(w http.ResponseWriter, r *http.Request){
+	log.Info().Msg("got /trigger_delay")
+	parms, err := url.ParseQuery(r.RequestURI)
+	if err != nil {
+		log.Warn().Msg(fmt.Sprintf("Unable to parse URI: %s ", r.RequestURI))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	_, err = io.WriteString(w, fmt.Sprintf("<h3>%s</h3>", parms.Get("/trigger_delay?q")))
+	if err != nil {
+		log.Warn().Msg("Unable to write")
+		w.WriteHeader(http.StatusInternalServerError)
+		return 
+	}
+}
+
 func getHello(w http.ResponseWriter, _ *http.Request) {
 	log.Info().Msg("got /hello request")
 	_, err := io.WriteString(w, "hello!")
 	if err != nil {
-		log.Info().Msg("unable to handle writing in getHello")
+		log.Warn().Msg("unable to handle writing in getHello")
 		return
 	}
 }
